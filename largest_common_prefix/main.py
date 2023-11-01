a02a5d40882c9fb55388f1b35afa66852e03229d
balazaigo <bala@zaigoinfotech.com>
2023-11-01 16:11:38 +0530

    Changes for the leet code.

--- largest_common_prefix/main.py
+++ largest_common_prefix/main.py
@@ -1,18 +1,32 @@
 class Solution(object):
     def longestCommonPrefix(self, strs):
         prefixes = {}
+        consider_one = False
         for string in strs:
+            if len(strs) == 1 and len(string) == 1:
+                consider_one = True
             for i in range(len(string)):
-                if len(string) - 1 != i:
-                    key = string[i] + string[i+1]
-                    if prefixes.get(key, None):
-                        prefixes[key] += 1
-                    else:
-                        prefixes[key] = 1
+                position = i+1
+                desired_prefix = string[:position]
+                detected_prefix = prefixes.get(desired_prefix, 0)
+                if detected_prefix:
+                    prefixes[desired_prefix] += 1
                 else:
-                    prefixes[string] = 1
-        print("prefixes", prefixes)
+                    prefixes[desired_prefix] = 1
+        largest_count = 0
+        largest_prefix = ""
+        for prefix, count in prefixes.items():
+            if (count > largest_count or (largest_count != 0 and count == largest_count)) and count == len(strs):
+                largest_count = count
+                largest_prefix = prefix
+        if largest_count == 0 or (not consider_one and largest_count == 1):
+            return ""
+        return largest_prefix
 
 
 sol = Solution()
-print("Method call", sol.longestCommonPrefix(["Bala", "Muruga", "Balance"]))
+# print("Method call", sol.longestCommonPrefix(["flower", "flow", "flight"]))
+# print("Method call", sol.longestCommonPrefix(["dog", "racecar", "car"]))
+# print("Method call", sol.longestCommonPrefix(["a"]))
+print("Method call", sol.longestCommonPrefix(
+    ["flower", "flower", "flower", "flower"]))
